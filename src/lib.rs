9b5795a13833d02bc94144dbb2e9cde2c3437efd
Dan Munckton <dan@cultivatehq.com>
2018-03-06 13:15:40 +0000

    Start writing ohms_value_to_float

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::num::ParseFloatError;
+
 #[derive(Debug, PartialEq)]
 pub struct ResistorCode(pub f32);
 
@@ -5,4 +7,8 @@ impl ResistorCode {
     pub fn new(value_string: &str) -> Result<Self, i32> {
         Ok(ResistorCode(value_string.parse().expect("failed")))
     }
+}
+
+pub fn ohms_value_to_float(value_string: &str) -> Result<f32, ParseFloatError> {
+    value_string.parse()
 }
--- tests/resistor_codes.rs
+++ tests/resistor_codes.rs
@@ -2,10 +2,23 @@ extern crate resistor_codes;
 #[macro_use] extern crate hamcrest;
 
 use hamcrest::prelude::*;
-use resistor_codes::ResistorCode;
+use resistor_codes::{ResistorCode, ohms_value_to_float};
 
 #[test]
 fn it_works() {
     assert_that!(ResistorCode::new("1").unwrap(), is(equal_to(ResistorCode(1.0))));
     assert_that!(ResistorCode::new("2").unwrap(), is(equal_to(ResistorCode(2.0))));
+}
+
+#[test]
+fn it_converts_from_ohms() {
+    assert_that!(ohms_value_to_float("1").unwrap(), is(equal_to(1.0)));
+    assert_that!(ohms_value_to_float("2").unwrap(), is(equal_to(2.0)));
+}
+
+#[test]
+#[ignore]
+fn it_converts_from_kilohms() {
+    assert_that!(ohms_value_to_float("1k").unwrap(), is(equal_to(1000.0)));
+    assert_that!(ohms_value_to_float("2K").unwrap(), is(equal_to(2000.0)));
 }
