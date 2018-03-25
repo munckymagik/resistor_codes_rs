e578e0cbeb557e2db72768809233c344c6e26c22
Dan Munckton <dan@cultivatehq.com>
2018-03-25 21:36:03 +0100

    Refactor test_float_value_to_ohms

--- tests/test_float_value_to_ohms.rs
+++ tests/test_float_value_to_ohms.rs
@@ -4,26 +4,30 @@ use resistor_codes::float_value_to_ohms;
 
 mod fixtures;
 
+fn check(input: f32, expected: &str) {
+    assert_eq!(float_value_to_ohms(input), expected);
+}
+
 #[test]
 fn test_it_assigns_correct_code_for_range() {
-    assert_eq!(float_value_to_ohms(0.0), "0R");
-    assert_eq!(float_value_to_ohms(0.001), "1L");
-    assert_eq!(float_value_to_ohms(1.0), "1R");
-    assert_eq!(float_value_to_ohms(1_000.0), "1K");
-    assert_eq!(float_value_to_ohms(1_000_000.0), "1M");
-    assert_eq!(float_value_to_ohms(1_000_000_000.0), "1G");
-    assert_eq!(float_value_to_ohms(1_000_000_000_000.0), "1T");
+    check(0.0, "0R");
+    check(0.001, "1L");
+    check(1.0, "1R");
+    check(1_000.0, "1K");
+    check(1_000_000.0, "1M");
+    check(1_000_000_000.0, "1G");
+    check(1_000_000_000_000.0, "1T");
 }
 
 #[test]
 fn test_defaults_for_unsupported_values() {
-    assert_eq!(float_value_to_ohms(0.000_001), "0.000001R");
-    assert_eq!(float_value_to_ohms(1_000_000_000_000_000.0), "1000T");
+    check(0.000_001, "0.000001R");
+    check(1_000_000_000_000_000.0, "1000T");
 }
 
 #[test]
 fn test_standard_e_series_resistor_values() {
-    for &(value, expected) in fixtures::E_SERIES_SAMPLES {
-        assert_eq!(float_value_to_ohms(value), expected);
+    for &(float_value, expected_ohms_str) in fixtures::E_SERIES_SAMPLES {
+        check(float_value, expected_ohms_str);
     }
 }
