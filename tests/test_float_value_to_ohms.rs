0be37546f3c26780455407e913be8c61b7776be3
Dan Munckton <dan@cultivatehq.com>
2018-03-25 21:17:42 +0100

    Add verbose test for standard resistor values

--- tests/test_float_value_to_ohms.rs
+++ tests/test_float_value_to_ohms.rs
@@ -2,6 +2,8 @@ extern crate resistor_codes;
 
 use resistor_codes::float_value_to_ohms;
 
+mod fixtures;
+
 #[test]
 fn test_it_assigns_correct_code_for_range() {
     assert_eq!(float_value_to_ohms(0.0), "0R");
@@ -17,4 +19,11 @@ fn test_it_assigns_correct_code_for_range() {
 fn test_defaults_for_unsupported_values() {
     assert_eq!(float_value_to_ohms(0.000_001), "0.000001R");
     assert_eq!(float_value_to_ohms(1_000_000_000_000_000.0), "1000T");
+}
+
+#[test]
+fn test_standard_e_series_resistor_values() {
+    for &(value, expected) in fixtures::E_SERIES_SAMPLES {
+        assert_eq!(float_value_to_ohms(value), expected);
+    }
 }
